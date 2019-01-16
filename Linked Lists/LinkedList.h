d88d7820d415c898ff3eef380253ac8351daff0d
Jamie Vasales <jamie.vasales@outlook.com>
2019-01-17 00:34:50 +1100

    Upload with working deletion (including repition)

--- Linked Lists/LinkedList.h
+++ Linked Lists/LinkedList.h
@@ -21,16 +21,7 @@ class LinkedList {
 
     void printList();
 
-    void display()
-  {
-    Node *temp=new Node;
-    temp=head;
-    while(temp!=nullptr)
-    {
-      cout<<temp->data<<"\t";
-      temp=temp->next;
-    }
-  } 
+    void deleteFromData(int);
 
 
 };
@@ -52,10 +43,44 @@ void LinkedList::insertData(int data) {
 
 void LinkedList::printList() {
     Node *current = new Node();
-    current = this->head;
+    current = head;
     while(current) {
         cout<<current->data<<",";
         current = current->next;
     }
     cout<<endl;
+}
+
+void LinkedList::deleteFromData(int data) {
+    Node *current = new Node();
+    Node *previous = new Node();
+    current = head;
+    if (current->data == data) {
+            head = head->next;
+        }
+    else
+    while(current!=nullptr) {
+
+        if(current == tail && current->data == data) {
+            tail = previous;
+            tail->next = nullptr;
+            return;
+        }
+
+            if (current->next->data == data) {
+                //temp = current->next;
+                // if(current->next!= nullptr) {
+                previous = current;
+                current->next = current->next->next;
+                // }
+                // else {
+                //     current->next = nullptr;
+                //     tail=current;
+                // }
+            }
+
+        if(current!=nullptr)
+        current = current->next;
+    }
+
 }
--- Linked Lists/main.cpp
+++ Linked Lists/main.cpp
@@ -11,7 +11,13 @@ int main() {
         linkedlist->insertData(36);
         linkedlist->insertData(72);
         linkedlist->insertData(108);
+        linkedlist->insertData(108);
 
+        linkedlist->printList();
+        linkedlist->deleteFromData(108);
+        linkedlist->deleteFromData(36);
+        linkedlist->printList();
+        linkedlist->insertData(5);
         linkedlist->printList();
         return 0;
 }
